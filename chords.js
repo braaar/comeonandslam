8fdf5e95b7b50ccc68a1d86058cf239ada1fe6e8
Brage Sekse Aarset <brage.aarset@gmail.com>
2020-10-05 16:09:54 +0200

    init

---
+++ chords.js
@@ -0,0 +1,35 @@
+const chords = {
+    "Chromatic": [
+        "C",
+        "Db",
+        "D",
+        "Eb",
+        "E",
+        "F",
+        "Gb",
+        "G",
+        "Ab",
+        "Bb",
+        "B"
+    ],
+    "C major": [
+        "C",
+        "Dm",
+        "Em",
+        "F",
+        "G",
+        "Am",
+        "B"
+    ],
+    "A minor": [
+        "Am",
+        "B",
+        "C",
+        "Dm",
+        "Em",
+        "F",
+        "G"
+    ]
+};
+
+const extensions = ["", "7", "Maj7", "m7", "\xB07", "sus2", "sus4", "6", "6/9", "9"];
---
+++ jammer.js
@@ -0,0 +1,45 @@
+"use strict";
+
+let chordButton = document.getElementById('chord-button');
+let undoButton = document.getElementById('undo-button');
+let resetButton = document.getElementById('reset-button');
+
+let keySelector = document.getElementById('chord-key')
+let chordField = document.getElementById('chord-field');
+let chordArray = [];
+
+
+chordButton.addEventListener('click', function(){addChord();});
+undoButton.addEventListener('click', function(){undoChord()});
+resetButton.addEventListener('click', function(){resetChords()});
+
+function addChord() {
+
+    chordArray.push(chords["Chromatic"][randomInteger(0, 6)] + extensions[randomInteger(0, extensions.length-1)]);
+    //TODO legge inn duplicate protection
+    chordField.textContent = printChords(chordArray);
+}
+
+function undoChord() {
+    chordArray.pop();
+    chordField.textContent = printChords(chordArray);
+}
+
+function resetChords() {
+    chordArray = [];
+    chordField.textContent ="";
+}
+
+function printChords(array) {
+    let result = "";
+    array.forEach(element => {
+        result += element + ' ';
+    });
+    return result;
+}
+
+function randomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+console.log('javascript loaded');
